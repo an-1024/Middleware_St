f486fd1b5d4a814db0f44de7edb914f38c59b9d7
azh <2550364626@qq.com>
2023-03-23 22:49:00 +0800

    Socket 三版优化

---
+++ netty/src/main/java/com/anzhi/tcpipconnect/WebSocketClientDemo.java
@@ -0,0 +1,58 @@
+package com.anzhi.tcpipconnect;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
+/**
+ * 单线程的 Socket 服务
+ */
+public class WebSocketClientDemo {
+    public static void main(String[] args) {
+        // 创建 socket 对象
+        Socket socket = null;
+        // 创建输入输出流
+        ObjectOutputStream outputStream = null;
+        ObjectInputStream inputStream = null;
+        // 服务器的通讯地址
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 20001);
+        try {
+            socket = new Socket();
+            // 连接服务器
+            socket.connect(inetSocketAddress);
+            System.out.println("connect Server success");
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            inputStream = new ObjectInputStream(socket.getInputStream());
+
+            System.out.println("ready send message");
+            // 向服务器输出信息
+            outputStream.writeUTF("Java");
+            outputStream.flush();
+
+            // 接收服务器的信息
+            System.out.println("receive message from server: " + inputStream.readUTF());
+        } catch (Exception e) {
+            // doNothing
+        } finally {
+            try {
+                if (!Objects.isNull(socket)) {
+                    socket.close();
+                }
+
+                if (!Objects.isNull(outputStream)) {
+                    outputStream.close();
+                }
+                
+                if(!Objects.isNull(inputStream)) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+
+            }
+        }
+    }
+}
---
+++ netty/src/main/java/com/anzhi/tcpipconnect/WebSocketServiceDemo.java
@@ -0,0 +1,59 @@
+package com.anzhi.tcpipconnect;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
+/**
+ * 单线程的 Socket 服务
+ */
+public class WebSocketServiceDemo {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket accept = null;
+        try {
+            serverSocket = new ServerSocket();
+            // 绑定监听端口
+            serverSocket.bind(new InetSocketAddress(20001));
+            System.out.println("Server start with Port: 20001");
+            int count = 0;
+            while (true) {
+                // 监听连接, 建立连接实例
+                accept = serverSocket.accept();
+                System.out.println("accept client socket.....total = " + (++count));
+
+                // 实例化与客户端的输入输出流
+                try (ObjectInputStream inputStream = new ObjectInputStream(accept.getInputStream());
+                     ObjectOutputStream outputStream = new ObjectOutputStream(accept.getOutputStream())) {
+                    
+                    // 接收客户端的输出
+                    String message = inputStream.readUTF();
+                    System.out.println("accept client message:" + message);
+                    
+                    // 服务端输出信息
+                    outputStream.writeUTF("hello" + message);
+                    outputStream.flush();
+                }
+
+            }
+        } catch (Exception e) {
+            // doNothing
+        }finally {
+            try {
+                if(!Objects.isNull(serverSocket)) {
+                    serverSocket.close();
+                }
+                
+                if (!Objects.isNull(accept)){
+                    accept.close();
+                }
+            }catch (Exception e){
+                // doNothing
+            }
+        }
+    }
+}
